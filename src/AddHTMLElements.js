7e977a1e799f03feca517e6807d735eac2cabdab
Darya Kondrina <dashakondrina61299@gmail.com>
2023-04-30 18:13:34 +0400

    feat: implement class AddHTMLElements and two method

---
+++ src/AddHTMLElements.js
@@ -0,0 +1,31 @@
+export default class AddHTMLElements {
+  body;
+
+  constructor(bodySelector) {
+    this.body = document.querySelector(bodySelector);
+  }
+
+  addHeader() {
+    const header = document.createElement('header');
+    header.classList.add('header');
+
+    const h1 = document.createElement('h1');
+    h1.classList.add('title');
+    h1.innerText = 'RSS Виртуальная клавиатура';
+
+    header.appendChild(h1);
+    this.body.appendChild(header);
+  }
+
+  addMain() {
+    const main = document.createElement('main');
+    main.classList.add('main');
+
+    const h2 = document.createElement('h2');
+    h2.classList.add('title');
+    h2.innerText = 'Тут будут кнопки';
+
+    main.appendChild(h2);
+    this.body.appendChild(main);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import '../assets/styles/style.css';
+
+import AddHTMLElements from './AddHTMLElements';
+
+const addHTMLElements = new AddHTMLElements('.general-body');
+
+console.log(addHTMLElements.body);
+
+addHTMLElements.addHeader();
+
+addHTMLElements.addMain();
