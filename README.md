a0633a69627c7af120d74519e05a76914c4c6ae1
Darya Kondrina <dashakondrina61299@gmail.com>
2023-05-02 00:30:32 +0400

    feat: deploy to gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# virtual-keyboard
-Virtual-Keyboard Task RS School
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const env = process.env.NODE_ENV || 'production';
-
-module.exports = (
-  env === 'production'
-    ? require('./webpack.prod.config')
-    : require('./webpack.dev.config')
-);
