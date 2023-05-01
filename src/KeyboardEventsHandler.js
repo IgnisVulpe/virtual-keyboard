bc74c039fc37e4d534e50c84b41e276335728a2b
Darya Kondrina <dashakondrina61299@gmail.com>
2023-05-01 23:12:35 +0400

    feat: full keydown handle with visual effect

--- src/KeyboardEventsHandler.js
+++ src/KeyboardEventsHandler.js
@@ -31,6 +31,10 @@ export default class KeyboardEventsHandler {
   keydownHandler(event) {
     console.log(event);
 
+    function press(key) {
+      key.classList.add('pressed');
+    }
+
     if (!this.exceptionSymbols.includes(event.key)) {
       const eventKey = this.alphabetsSymbolsUpperCase.includes(event.key)
         ? event.key.toLowerCase()
@@ -39,7 +43,119 @@ export default class KeyboardEventsHandler {
       const pressedKey = document.querySelector(
         `button[data-key="${eventKey}"]`,
       );
-      pressedKey.style.backgroundColor = 'green';
+      press(pressedKey);
+    } else if (this.exceptionSymbols.includes(event.key)) {
+      switch (event.key) {
+        case '\\': {
+          const pressedKey = document.querySelector(
+            'button[data-key="&#92;"]',
+          );
+          press(pressedKey);
+          break;
+        }
+        case 'Delete': {
+          const pressedKey = document.querySelector(
+            'button[data-key="Del"]',
+          );
+          press(pressedKey);
+          break;
+        }
+        case 'CapsLock': {
+          const pressedKey = document.querySelector(
+            'button[data-key="Caps Lock"]',
+          );
+          press(pressedKey);
+          break;
+        }
+        case 'Shift': {
+          let pressedKey = null;
+          if (event.code === 'ShiftLeft') {
+            pressedKey = document.querySelector(
+              'button[data-key="Shift"]',
+            );
+          } else {
+            [, pressedKey] = document.querySelectorAll('button[data-key="Shift"]');
+          }
+          press(pressedKey);
+          break;
+        }
+        case '/': {
+          const pressedKey = document.querySelector(
+            'button[data-key="&#47;"]',
+          );
+          press(pressedKey);
+          break;
+        }
+        case 'Control': {
+          let pressedKey = null;
+          if (event.code === 'ControlLeft') {
+            pressedKey = document.querySelector(
+              'button[data-key="Ctrl"]',
+            );
+          } else {
+            [, pressedKey] = document.querySelectorAll('button[data-key="Ctrl"]');
+          }
+          press(pressedKey);
+          break;
+        }
+        case 'Alt': {
+          let pressedKey = null;
+          if (event.code === 'AltLeft') {
+            pressedKey = document.querySelector(
+              'button[data-key="Alt"]',
+            );
+          } else {
+            [, pressedKey] = document.querySelectorAll('button[data-key="Alt"]');
+          }
+          press(pressedKey);
+          break;
+        }
+        case ' ': {
+          const pressedKey = document.querySelector(
+            'button[data-key="Space"]',
+          );
+          press(pressedKey);
+          break;
+        }
+        case 'ArrowUp': {
+          const pressedKey = document.querySelector(
+            'button[data-key="&#9650;"]',
+          );
+          press(pressedKey);
+          break;
+        }
+        case 'ArrowDown': {
+          const pressedKey = document.querySelector(
+            'button[data-key="&#9660;"]',
+          );
+          press(pressedKey);
+          break;
+        }
+        case 'ArrowLeft': {
+          const pressedKey = document.querySelector(
+            'button[data-key="&#9668;"]',
+          );
+          press(pressedKey);
+          break;
+        }
+        case 'ArrowRight': {
+          const pressedKey = document.querySelector(
+            'button[data-key="&#9658;"]',
+          );
+          press(pressedKey);
+          break;
+        }
+        case '': {
+          const pressedKey = document.querySelector(
+            'button[data-key=""]',
+          );
+          press(pressedKey);
+          break;
+        }
+        default: {
+          break;
+        }
+      }
     }
   }
 
